3316dc52143c75a2e0bca381165b82d2427c00f8
TimJoePat <TimJoePat88@cox.net>
2024-01-08 21:53:55 -0700

    302 Complete

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import "./styles.css";
-import { useState } from "react";
-
-export default function App() {
-  return <div>Emty React App</div>;
-}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import App from "./App";
+import App from "./components/App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+root.render(<App />);
+
+//1. Apply CSS styles to App.jsx component
+//to match the appearance on the completed app:
+//https://c6fkx.csb.app/
+//3. Use props to render the default Beyonce contact card.
+//4. Use the contacts.js file to create card components.
