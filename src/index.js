dd084d57f9bfd96b7d7545e2f47720479ba34bf8
TimJoePat <TimJoePat88@cox.net>
2024-02-01 06:35:25 -0700

    202 Bank Account challenge in progress - but it runs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import App from "./App";
 
-//1. Apply CSS styles to App.jsx component
-//to match the appearance on the completed app:
-//https://c6fkx.csb.app/
-//3. Use props to render the default Beyonce contact card.
-//4. Use the contacts.js file to create card components.
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
